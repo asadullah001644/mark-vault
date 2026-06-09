dca52b966ee7b8935b603b0f95e5fafe29411999
Muhammad Asadullah <muhammadasadullah833@gmail.com>
2026-06-09 18:44:15 +0500

    create mongodb connection and user schema

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
 import  express  from "express";
+import connectDB from "./config/db";
+import dotenv from 'dotenv';
 
+
+dotenv.config()
+connectDB();
 const app = express();
+
+app.listen(8000, ()=>{
+    console.log("Server is running on port 8000");
+})
